9190c95c9079072032ddb29fd60f37220ec8ed2c
jithinem <jithinem4007@gmail.com>
2023-04-01 20:22:21 +0530

    demo app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Testfunction } from "./components/Testfunction";
+import  Testclass  from "./components/Testclass";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Drink</h1>
+        <ul>
+          <li>Tea</li>
+          <li>Coffee</li>
+          <li>Milk</li>
+        </ul>
+        <Testfunction/>
+        <Testclass/>
       </header>
     </div>
   );
